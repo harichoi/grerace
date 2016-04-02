fb73160e44c54287de9fd2f889e549e2fdf3578b
iladnuk <cjs004@korea.com>
2016-04-02 17:14:23 +0900

    first commit

---
+++ resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by grerace.rc
+//
+#define IDB_WATER                       101
+#define IDB_GROUND                      102
+#define IDB_SLOW                        103
+#define IDB_WALL                        104
+#define IDB_CAR                         105
+#define IDB_CARMASK                     106
+#define IDB_FINISH                      107
+#define IDB_FAST                        108
+#define IDB_MADEBY                      109
+#define IDB_LEVELMASK                   110
+#define IDB_EXPLAIN                     111
+#define IDB_LEVEL                       112
+#define IDB_BG                          113
+#define IDB_LIMIT                       114
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
